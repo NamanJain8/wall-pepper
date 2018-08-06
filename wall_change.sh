a4a0fa6c8403345ede561684e7f442e709c45962
Naman Jain bt cse <namanj@ccpc143.cc.iitk.ac.in>
2018-08-06 15:22:10 +0530

    Cronjob working

--- wall_change.sh
+++ wall_change.sh
@@ -8,7 +8,9 @@
 # PWD=/root
 
 # rm *.jpg
-rm ~/apod/*.jpg ~/apod/*wallp* ~/apod/2* ~/apod/*.txt
+PID=$(pgrep gnome-session)
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-)
+rm ~/apod/*wallp* ~/apod/2* ~/apod/*.txt
 user=$(whoami)
 cd ~/apod
 rm -f *wallpaper* 2*
@@ -20,7 +22,8 @@ cat 2 | tr '><' '>\n<' | grep 'href="/static/images' >> temp.txt
 b=$(gawk -F "/" '{split($4,b,"\""); print b[1]}' temp.txt)
 c=$(echo $b | tr ' ' '\n' | shuf -n 1)
 d=$a$c
-wget $d
+wget -O "new.jpg" $d
 imname=$(ls | egrep '\.png$|\.jpg$')
-/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///$(pwd)/$imname
+/usr/bin/gsettings set org.gnome.desktop.background picture-uri file:///$(pwd)/"new.jpg"
 # rm -f temp.txt *wallp* 2
+
